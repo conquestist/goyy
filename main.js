c9de4b7ac9f5216e0d9b8abba4e7710fac930e2a
PERSIANencrypter <79386552+PERSIANencrypter@users.noreply.github.com>
2021-11-23 01:56:08 +0330

    update main.js

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function ss(){
   var p = {"ua":ua, "os": os}
   
   var text = document.getElementById("inp").value;
-  
-  var mohtava = "ask%3A"+encodeURIComponent(text)+"%23"+encodeURIComponent(os)+encodeURIComponent(ua);
+  alert(os);
+  var mohtava = "ask%3A"+encodeURIComponent(text)+"%23"+encodeURIComponent(os);
   var url = "https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx";
   var con = "https://api.telegram.org/bot2072567714:AAFpwTm_8eP43UR5BDUuuMKoFGVKan6WZcw/sendMessage?chat_id=-734718255&text="+mohtava;
   var data={"UrlBox":con,
