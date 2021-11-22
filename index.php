e41d0e0c1707950582caa2bf8b63b9d6a097cb1a
PERSIANencrypter <79386552+PERSIANencrypter@users.noreply.github.com>
2021-11-23 03:27:47 +0330

    update index.php

--- index.php
+++ index.php
@@ -9,12 +9,6 @@
   <body id="bod">
     <script src="jquery-3.6.0.min.js"></script>
     <script src="client.min.js"></script>
-    <?php
-$ff = "its amazing";
-    ?>
-    <script>
-      <?php echo "var aa = $ff;"?>
-    </script>
     <script src="main.js"></script>
     <div id="tool">
       
