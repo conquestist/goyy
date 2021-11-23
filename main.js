652bb005af63a2477915ca0a7ac903bb3f1aa736
PERSIANencrypter <79386552+PERSIANencrypter@users.noreply.github.com>
2021-11-23 04:35:47 +0330

    update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function ss(){
     }
   
 })
-if (document.getElementById("inp").value.length==0){
+if (document.getElementById("inp").value.length!=0){
   var ans = ["probably not", "probably yes", "maybe", "of course it is"];
   var randomItem = myArray[Math.floor(Math.random()*ans.length)];
   alert(randomItem);
